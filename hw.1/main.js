d68665fe7cee6dd656bc929f15439734867119b7
Dench1k28 <morozdenis00@gmail.com>
2026-02-09 19:20:35 +0200

    new

---
+++ hw.1/main.js
@@ -0,0 +1,48 @@
+/*----------------------------1---------------------------------*/
+let a = 'hello';
+console.log(a);
+
+let b = 'owu';
+console.log(b);
+
+let c = 'com';
+console.log(c);
+
+let d = 'ua';
+console.log(d);
+
+let e = 1;
+console.log(e);
+
+let f = 10;
+console.log(f);
+
+let g = -999;
+console.log(g);
+
+let i = 3.14;
+console.log(i);
+
+let j = (!!12);
+console.log(j);
+
+let k = (!!0);
+console.log(k);
+/*-----------------------------2----------------------------------*/
+
+let firstName = 'Denys';
+let middleName = 'Moroz';
+let lastName = 'Yuriyovych';
+
+let m = `${firstName} ${middleName} ${lastName}`;
+console.log(m);
+/*-----------------------------3-------------------------------------*/
+
+let x = 100;
+console.log(typeof x);
+
+let z = '100';
+console.log(typeof z);
+
+let y = true;
+console.log(typeof y);
