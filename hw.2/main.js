faa8382c10c910080360a4f72d00fca0fbe22b36
Dench1k28 <morozdenis00@gmail.com>
2026-02-16 14:09:25 +0200

    all

---
+++ hw.2/main.js
@@ -0,0 +1,206 @@
+//--------------1-----------------//
+
+/*let arr = [1, 2, 100, -45, true, false, 'okten', 'school', [], 5];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);*/
+
+/*-----------------2--------------------*/
+
+/*let book1 ={title:'1',pageCount:'50',genre:'roman'};
+let book2 ={title:'2',pageCount:'39',genre:'triller'};
+let book3 ={title:'3',pageCount:'100',genre:'comedy'};
+
+console.log(book1);
+console.log(book2);
+console.log(book3);*/
+
+/*-------------3-------------------------*/
+
+
+
+/*
+let books = {title:'1',pageCount:50,genre:'roman',authors:[{name:'vasya',age:34}, {name:'alina',age:56}, {name:'vitya',age:23}]};
+console.log(books);
+*/
+
+/*-----------------------4----------------------------*/
+
+/*let users =[
+{user:'ad', username:'erhtr', password:'123'},
+{user:'afaf', username:'5hgd', password:'432'},
+{user:'hht', username:'jrv', password:'653'},
+{user:'4ewf', username:'fds45', password:'745'},
+{user:'vvn', username:'j4r', password:'16'},
+{user:'wem', username:'q53', password:'363'},
+{user:'udfe', username:'d3bs', password:'85'},
+{user:'bvxvbw', username:'sr4', password:'234'},
+{user:'rtnfwe', username:'424s', password:'6754'},
+{user:'cvwqey', username:'234w', password:'521'}
+
+];
+
+console.log(users[0]);
+console.log(users[1]);
+console.log(users[2]);
+console.log(users[3]);
+console.log(users[4]);
+console.log(users[5]);
+console.log(users[6]);
+console.log(users[7]);
+console.log(users[8]);
+console.log(users[9]);*/
+
+
+
+/*--------------5----------------------*/
+
+
+/*let temps =[
+{day:'Monday', values:{morning:15,day:20,evening:17}},
+{day:'Tuesday', values:{morning:15,day:20,evening:17}},
+{day:'Wednesday', values:{morning:15,day:20,evening:17}},
+{day:'Thursday', values:{morning:15,day:20,evening:17}},
+{day:'Friday', values:{morning:15,day:20,evening:17}},
+{day:'Saturday', values:{morning:15,day:20,evening:17}},
+{day:'Sunday', values:{morning:15,day:20,evening:17}},
+
+];
+console.log(temps);*/
+
+/*-----------6--------------------------------*/
+
+/*let x = +prompt('enter number 1 0 -3');//if you wrote 'prompt' without + it is string, if you wrote with + it is number
+if(x!==0){
+console.log('Right');
+}else{
+console.log('Wrong');
+}*/
+
+//--------------7----------------------//
+
+/*let time =37;
+if (time >=0 && time<15){
+console.log('part1');
+}
+else if (time >=15 && time<30){
+console.log('part2');
+}
+else if (time >=30 && time<45){
+console.log('part3');
+}
+else if (time >=45 && time<60){
+console.log('part4');
+}*/
+//------------------8-----------------------//
+
+/*let day = prompt('enter number');
+if (day >=1 && day <10){
+console.log('first');
+}
+else if (day >=10 && day <20){
+console.log('second');
+}
+else if(day >=20 && day <=31){
+console.log('third');
+}*/
+//-----------------9------------------------------//
+
+/*let day = prompt('enter day of week');
+
+switch (day){
+case '1':
+console.log('Monday');
+break;
+case '2':
+console.log('Tuesday');
+break;
+case '3':
+console.log('Wednesday');
+break;
+case '4':
+console.log('Thursday');
+break;
+case '5':
+console.log('Friday');
+break;
+case '6':
+console.log('Saturday');
+break;
+case '7':
+console.log('Sunday');
+break;
+default:
+console.log('?????');
+}*/
+
+//----------------10-------------------------//
+/*let first = +prompt('enter the first number');
+let second =+prompt('second number');
+if (first<second){
+console.log(second);
+}else if(first>second){
+console.log(first);
+}else if(first==second){
+console.log('the same');
+}*/
+//-------------------11------------------------//
+
+// 0
+// ''
+// false
+// null
+// undefined
+//Nan
+/*let x = 0;
+if (!x){
+x = 'default'
+}*/
+
+//-------------------12-------------------------//
+
+
+/*let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+let a = coursesAndDurationArray;
+let b = (a[0]);
+let c = (a[1]);
+let d = (a[2]);
+let e = (a[3]);
+let f = (a[4]);
+let g = (a[5]);
+
+let number1 =(b[1]);
+let number2 =(c[1]);
+let number3 =(d[1]);
+let number4 =(e[1]);
+let number5 =(f[1]);
+let number6 =(g[1]);
+let x = prompt('enter number of course');
+if (number1, number2, number3, number4, number5, number6 >5){
+console.log('Super');
+}else if(number1, number2, number3, number4, number5, number6 <=5){
+console.log('Not Super');
+}*/
+
+
+
+
+
+
+
