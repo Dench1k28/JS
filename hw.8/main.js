7880f816994a370591b4f9428f9d506429daf017
Dench1k28 <morozdenis00@gmail.com>
2026-03-26 10:45:36 +0200

    all +++

---
+++ hw.8/main.js
@@ -0,0 +1,43 @@
+//--------------------------1---------------------------//
+/*let coursesAndDurationArray = [
+
+    {title: 'JavaScript Complex', monthDuration: 5},
+
+    {title: 'Java Complex', monthDuration: 6},
+
+    {title: 'Python Complex', monthDuration: 6},
+
+    {title: 'QA Complex', monthDuration: 4},
+
+    {title: 'FullStack', monthDuration: 7},
+
+    {title: 'Frontend', monthDuration: 4}
+
+];
+console.log(coursesAndDurationArray.map((course, index)=>({...course,id:index+1})));*/
+
+//---------------------------------2----------------------------//
+
+function cloner(obj){
+if(obj){
+let functions =[];
+for (const key in obj){
+if(typeof obj[key] === 'function'){
+const functionClone = obj[key].bind({});
+functions.push(functionClone);
+}
+}
+const cloneObj = JSON.parse(JSON.stringify(obj));
+for (const func of functions){
+cloneObj[func.key] = func.functionClone
+}
+console.log(cloneObj);
+return cloneObj
+}
+throw new Error('!!!!');
+
+}
+
+const clone =cloner({id:123,name:'dasd',greeting() {console.log('hello')}, foo() {console.log('bar')}});
+clone.foo();
+
